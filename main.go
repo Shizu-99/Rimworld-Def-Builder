025800b923efc9d6190083f8f1aef38188237bbd
Shizu-99 <flew1440@gmail.com>
2026-02-04 20:49:49 +1300

    Add testing for the xml marshalling

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"encoding/xml"
+	"fmt"
 
+	"github.com/Shizu-99/Rimworld-Def-Builder/models"
+)
+
+func main() {
+	parts := []string{
+		"Eye",
+		"Leg",
+		"Arm",
+	}
+	defList := []any{}
+	for _, part := range parts {
+		Hediff := &models.HediffDef{
+			DefName:    part,
+			ParentName: "Makable",
+		}
+		Thing := &models.ThingDef{
+			DefName:    part,
+			ParentName: "Makable",
+		}
+		Install := &models.RecipeDef{
+			DefName:    part,
+			ParentName: "Install",
+		}
+		Remove := &models.RecipeDef{
+			DefName:    part,
+			ParentName: "Remove",
+		}
+		defList = append(defList, Hediff, Thing, Install, Remove)
+	}
+	defs := models.Defs{
+		Defs: defList,
+	}
+	out, _ := xml.MarshalIndent(defs, " ", " ")
+	fmt.Println(string(out))
 }
