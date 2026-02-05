88490f1418feac1e4c0ab108c0861f48f9e33ff3
Shizu-99 <flew1440@gmail.com>
2026-02-05 20:19:02 +1300

    Add testing for new and extended structs.

--- main.go
+++ main.go
@@ -15,9 +15,41 @@ func main() {
 	}
 	defList := []any{}
 	for _, part := range parts {
+		stats := &models.StatOffsets{
+			ConstructionSpeedFactor: 0.1,
+			MiningSpeed:             1.5,
+		}
+		props := &models.PartProps{
+			Solid: true,
+		}
+		caps := &models.CapMods{
+			Capacities: []models.Capacity{
+				{
+					Capacity: "Sight",
+					Offset:   0.05,
+				},
+				{
+					Capacity: "Moving",
+					Offset:   0.5,
+				},
+			},
+		}
+		stages := &models.Stages{
+			Stats:        *stats,
+			CapacityMods: *caps,
+		}
 		Hediff := &models.HediffDef{
-			DefName:    part,
-			ParentName: "Makable",
+			DefName:               part,
+			ParentName:            "Makable",
+			Label:                 part,
+			LabelNoun:             "an " + part,
+			Description:           "An " + part + ", its a part of the body.",
+			DescriptionHyperlinks: part,
+			SpawnThingOnRemoved:   part,
+			AddedPartProps: []any{
+				props,
+			},
+			Stages: *stages,
 		}
 		Thing := &models.ThingDef{
 			DefName:    part,
@@ -36,6 +68,6 @@ func main() {
 	defs := models.Defs{
 		Defs: defList,
 	}
-	out, _ := xml.MarshalIndent(defs, " ", " ")
+	out, _ := xml.MarshalIndent(defs, " ", "	")
 	fmt.Println(string(out))
 }
