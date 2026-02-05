7b2d638be1a6afe97a2ccb47d508f236e942eca7
Shizu-99 <flew1440@gmail.com>
2026-02-05 20:18:29 +1300

    Add and extend structs to represent additional xml tags for Hediffs

--- models/models.go
+++ models/models.go
@@ -3,9 +3,16 @@ package models
 import "encoding/xml"
 
 type HediffDef struct {
-	XMLName    xml.Name `xml:"HediffDef"`
-	DefName    string   `xml:"defName"`
-	ParentName string   `xml:"ParentName,attr"`
+	XMLName               xml.Name `xml:"HediffDef"`
+	DefName               string   `xml:"defName"`
+	ParentName            string   `xml:"ParentName,attr"`
+	Label                 string   `xml:"label"`
+	LabelNoun             string   `xml:"labelNoun"`
+	Description           string   `xml:"description"`
+	DescriptionHyperlinks string   `xml:"descriptionHyperlinks>ThingDef"`
+	SpawnThingOnRemoved   string   `xml:"spawnThingOnRemoved"`
+	AddedPartProps        []any    `xml:"addedPartProps,omitempty"`
+	Stages                Stages   `xml:"stages>li,omitempty"`
 }
 
 type ThingDef struct {
@@ -22,4 +29,29 @@ type RecipeDef struct {
 
 type Defs struct {
 	Defs []any
+}
+
+type PartProps struct {
+	Solid bool `xml:"solid,omitempty"`
+}
+
+type StatOffsets struct {
+	XMLName                 xml.Name `xml:"statOffsets"`
+	ConstructionSpeedFactor float32  `xml:"ConstructionSpeedFactor,omitempty"`
+	MiningSpeed             float32  `xml:"MiningSpeed,omitempty"`
+}
+
+type CapMods struct {
+	XMLName    xml.Name   `xml:"capMods"`
+	Capacities []Capacity `xml:"li"`
+}
+
+type Capacity struct {
+	Capacity string  `xml:"capacity"`
+	Offset   float32 `xml:"offset"`
+}
+
+type Stages struct {
+	Stats        StatOffsets
+	CapacityMods CapMods
 }
