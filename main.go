c1b63d71195698e56773705ca889ad5378212c39
Shizu-99 <flew1440@gmail.com>
2026-02-06 15:44:28 +1300

    Comment out fyne call.

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
+	//"fyne.io/fyne/v2/app"
+	//"fyne.io/fyne/v2/widget"
 
 	"github.com/Shizu-99/Rimworld-Def-Builder/defMethods"
 )
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("Hello World")
+	//a := app.New()
+	//w := a.NewWindow("Hello World")
 
-	w.SetContent(widget.NewLabel("Hello World!"))
-	w.ShowAndRun()
+	//w.SetContent(widget.NewLabel("Hello World!"))
+	//w.ShowAndRun()
 
 	defMethods.CreateBodyPart()
 }
